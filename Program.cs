28772d63e3b90bef0a56ec012b136d415d789490
hfr4 <30574115+hfr4@users.noreply.github.com>
2024-07-11 14:08:28 +0200

    add moving background

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 class Program
 {
@@ -7,6 +8,12 @@ class Program
         Raylib.InitWindow(800, 480, "Pokédex");
 
         Texture2D background = Raylib.LoadTexture("Data/Images/background.png");
+
+        Texture2D normal    = Raylib.LoadTexture("Data/Images/normal.png");
+        Texture2D electric  = Raylib.LoadTexture("Data/Images/electric.png");
+        Texture2D fire      = Raylib.LoadTexture("Data/Images/fire.png");
+        Texture2D water     = Raylib.LoadTexture("Data/Images/water.png");
+        Texture2D grass     = Raylib.LoadTexture("Data/Images/grass.png");
 
         Texture2D pikachu    = Raylib.LoadTexture("Data/Images/pikachu.png");
         Texture2D charmander = Raylib.LoadTexture("Data/Images/charmander.png");
@@ -21,11 +28,9 @@ class Program
         Texture2D caterpie   = Raylib.LoadTexture("Data/Images/caterpie.png");
         Texture2D magmar     = Raylib.LoadTexture("Data/Images/magmar.png");
 
-        Texture2D normal    = Raylib.LoadTexture("Data/Images/normal.png");
-        Texture2D electric  = Raylib.LoadTexture("Data/Images/electric.png");
-        Texture2D fire      = Raylib.LoadTexture("Data/Images/fire.png");
-        Texture2D water     = Raylib.LoadTexture("Data/Images/water.png");
-        Texture2D grass     = Raylib.LoadTexture("Data/Images/grass.png");
+
+        Vector2 bg_offset = new Vector2(0, 0);
+
 
         while (!Raylib.WindowShouldClose())
         {
@@ -34,14 +39,30 @@ class Program
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
-            Raylib.DrawTexture(background, 0               , 0, new Color(255, 255, 255, 30));
-            Raylib.DrawTexture(background, background.Width, 0, new Color(255, 255, 255, 30));
+            // Draw moving background
+            {
+                bg_offset.X += (float) 0.05;
+                bg_offset.Y += (float) 0.05;
+        
+                if (bg_offset.X > background.Width)  bg_offset.X = 0;
+                if (bg_offset.Y > background.Height) bg_offset.Y = 0;
+
+                var bg_color = new Color(255, 255, 255, 30);
+    
+                Raylib.DrawTexture(background, (int) bg_offset.X - background.Width, (int) bg_offset.Y - background.Height, bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X - background.Width, (int) bg_offset.Y                    , bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X - background.Width, (int) bg_offset.Y + background.Height, bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X                   , (int) bg_offset.Y - background.Height, bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X                   , (int) bg_offset.Y                    , bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X                   , (int) bg_offset.Y + background.Height, bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X + background.Width, (int) bg_offset.Y - background.Height, bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X + background.Width, (int) bg_offset.Y                    , bg_color);
+                Raylib.DrawTexture(background, (int) bg_offset.X + background.Width, (int) bg_offset.Y + background.Height, bg_color);
+            }
 
             Raylib.DrawText("Pokédex !", 12, 12, 20, Color.Black);
 
             Raylib.DrawTexture(pikachu   , 200, 200, Color.White);
-
-
             Raylib.DrawTexture(charmander, 300, 200, Color.White);
             Raylib.DrawTexture(squirtle  , 400, 200, Color.White);
             Raylib.DrawTexture(bulbasaur , 500, 200, Color.White);
@@ -54,16 +75,103 @@ class Program
             Raylib.DrawTexture(caterpie  , 600, 300, Color.White);
             Raylib.DrawTexture(magmar    , 700, 300, Color.White);
 
+            Raylib.DrawText("All"        , 50 , 120, 20, Color.Black);
             Raylib.DrawTexture(normal    , 100, 100, Color.White);
             Raylib.DrawTexture(electric  , 200, 100, Color.White);
             Raylib.DrawTexture(fire      , 300, 100, Color.White);
             Raylib.DrawTexture(water     , 400, 100, Color.White);
             Raylib.DrawTexture(grass     , 500, 100, Color.White);
 
-            var pikachu_rect = new Rectangle(200, 200, pikachu.Width, pikachu.Height);
-            if (Raylib.CheckCollisionPointRec(mouse_position, pikachu_rect)) {
-                Raylib.DrawText("Pikachu !", (int) mouse_position.X, (int) mouse_position.Y, 20, Color.Black);
+            {
+                string element_name = "Normal";
+                string element_desc = "This is a normal element.";
+
+                var element_rect = new Rectangle(100, 100, normal.Width, normal.Height);
+                if (Raylib.CheckCollisionPointRec(mouse_position, element_rect)) {
+                    var text_rect = new Rectangle( (int) mouse_position.X + 15, (int) mouse_position.Y + 15, 300, 100);
+                    Raylib.DrawRectangleRec(text_rect, Color.White);
+                    Raylib.DrawRectangleLinesEx(text_rect, 1, Color.Black);
+                    Raylib.DrawText(element_name, (int) text_rect.X + 5, (int) text_rect.Y + 5     , 20, Color.Black);
+                    Raylib.DrawText(element_desc, (int) text_rect.X + 5, (int) text_rect.Y + 5 + 25, 10, Color.Gray);
+
+                    if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+                        Console.WriteLine("Press Normal !");
+                    }
+                }
             }
+
+            {
+                string element_name = "Electric";
+                string element_desc = "This is a electic element.";
+
+                var element_rect = new Rectangle(200, 100, electric.Width, electric.Height);
+                if (Raylib.CheckCollisionPointRec(mouse_position, element_rect)) {
+                    var text_rect = new Rectangle( (int) mouse_position.X + 15, (int) mouse_position.Y + 15, 300, 100);
+                    Raylib.DrawRectangleRec(text_rect, Color.White);
+                    Raylib.DrawRectangleLinesEx(text_rect, 1, Color.Black);
+                    Raylib.DrawText(element_name, (int) text_rect.X + 5, (int) text_rect.Y + 5     , 20, Color.Black);
+                    Raylib.DrawText(element_desc, (int) text_rect.X + 5, (int) text_rect.Y + 5 + 25, 10, Color.Gray);
+                }
+                
+                if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+                    Console.WriteLine("Press Electric !");
+                }
+            }
+
+            {
+                string element_name = "Fire";
+                string element_desc = "This is a fire element.";
+
+                var element_rect = new Rectangle(300, 100, fire.Width, fire.Height);
+                if (Raylib.CheckCollisionPointRec(mouse_position, element_rect)) {
+                    var text_rect = new Rectangle( (int) mouse_position.X + 15, (int) mouse_position.Y + 15, 300, 100);
+                    Raylib.DrawRectangleRec(text_rect, Color.White);
+                    Raylib.DrawRectangleLinesEx(text_rect, 1, Color.Black);
+                    Raylib.DrawText(element_name, (int) text_rect.X + 5, (int) text_rect.Y + 5     , 20, Color.Black);
+                    Raylib.DrawText(element_desc, (int) text_rect.X + 5, (int) text_rect.Y + 5 + 25, 10, Color.Gray);
+                }
+                
+                if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+                    Console.WriteLine("Press Fire !");
+                }
+            }
+
+            {
+                string element_name = "Water";
+                string element_desc = "This is a water element.";
+
+                var element_rect = new Rectangle(400, 100, water.Width, water.Height);
+                if (Raylib.CheckCollisionPointRec(mouse_position, element_rect)) {
+                    var text_rect = new Rectangle( (int) mouse_position.X + 15, (int) mouse_position.Y + 15, 300, 100);
+                    Raylib.DrawRectangleRec(text_rect, Color.White);
+                    Raylib.DrawRectangleLinesEx(text_rect, 1, Color.Black);
+                    Raylib.DrawText(element_name, (int) text_rect.X + 5, (int) text_rect.Y + 5     , 20, Color.Black);
+                    Raylib.DrawText(element_desc, (int) text_rect.X + 5, (int) text_rect.Y + 5 + 25, 10, Color.Gray);
+                }
+                
+                if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+                    Console.WriteLine("Press Water !");
+                }
+            }
+
+            {
+                string element_name = "Grass";
+                string element_desc = "This is a grass element.";
+                
+                var element_rect = new Rectangle(500, 100, grass.Width, grass.Height);
+                if (Raylib.CheckCollisionPointRec(mouse_position, element_rect)) {
+                    var text_rect = new Rectangle( (int) mouse_position.X + 15, (int) mouse_position.Y + 15, 300, 100);
+                    Raylib.DrawRectangleRec(text_rect, Color.White);
+                    Raylib.DrawRectangleLinesEx(text_rect, 1, Color.Black);
+                    Raylib.DrawText(element_name, (int) text_rect.X + 5, (int) text_rect.Y + 5     , 20, Color.Black);
+                    Raylib.DrawText(element_desc, (int) text_rect.X + 5, (int) text_rect.Y + 5 + 25, 10, Color.Gray);
+                }
+                
+                if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+                    Console.WriteLine("Press Grass !");
+                }
+            }
+
 
             Raylib.EndDrawing();
         }
@@ -76,7 +184,7 @@ class Pokemon
 {
     int PokemonId { get; set; }
     string Name { get; set; }
-    string Image { get; set; }
+    Texture2D Image { get; set; }
 
     public virtual Type Type { get; set; }
 }
@@ -86,7 +194,7 @@ class Type
     int TypeId { get; set; }
     string Name { get; set; }
     string Description { get; set; }
-    string Image { get; set; }
+    Texture2D Image { get; set; }
 
     public virtual List<Pokemon> Pokemons { get; set; }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+## Build
+
+dotnet run
